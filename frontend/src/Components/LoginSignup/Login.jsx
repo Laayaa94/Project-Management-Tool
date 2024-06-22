6bf73154b7c5baa5e9d1b703f03e8c474e5f5b7d
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-22 11:56:02 +0530

    backend

--- frontend/src/Components/LoginSignup/Login.jsx
+++ frontend/src/Components/LoginSignup/Login.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [state,setState]=useState("Sign Up");
   return (
     <div className='login-container'>
-      
+      <h1>{state}</h1>
+      {state=== "Sign Up" ? <input type="text" placeholder='Name' /> : <></>}
+
+        <input type="email" placeholder='Email'/>
+        <input type="password"  placeholder='Password'/>
+        <button>Continue</button>
+        
     </div>
   )
 }
